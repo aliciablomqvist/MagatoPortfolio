49b724cff91eb06015540876488df8483536cfad
Alicia Blomqvist <122750659+aliciablomqvist@users.noreply.github.com>
2025-04-10 20:20:29 +0200

    Model added

---
+++ Magato.Api/Models/PageContent.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Magato.Api.Models;
+public class PageContent
+{
+    public int Id { get; set; }
+    public string Key { get; set; } = null!; // Såsom "AboutMe", "StartPage" osv
+    public string Value { get; set; } = null!;
+}
