f97d964dea742197dcab114659131a28b19e4034
Alicia Blomqvist <122750659+aliciablomqvist@users.noreply.github.com>
2025-04-10 19:00:31 +0200

    Remove workflow file

--- Magato.Api/obj/Debug/net9.0/Magato.Api.AssemblyInfo.cs
+++ Magato.Api/obj/Debug/net9.0/Magato.Api.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925d91c78ed737e6fd5aea72bf67c549bfe47bd0")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304611d7a56dee9d090fbed995917848c6aa6e04")]
->>>>>>> feature/ContactForm
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b50cf4578d41d21b91d9ec69f5e8b90832afb233")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
