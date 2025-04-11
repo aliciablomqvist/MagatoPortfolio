86b78a16146fd8394871151f0448357c5119c9b9
Alicia Blomqvist <122750659+aliciablomqvist@users.noreply.github.com>
2025-04-11 09:46:07 +0200

    Model and DTO update for more functionality

--- Magato.Api/DTOs/PageContentDTO.cs
+++ Magato.Api/DTOs/PageContentDTO.cs
@@ -1,5 +1,16 @@
 public class PageContentDto
 {
     public string Key { get; set; } = null!;
-    public string Value { get; set; } = null!;
+    public string Title { get; set; } = string.Empty;
+
+    public string? MainText { get; set; } = null;
+    public string? SubText { get; set; } = null;
+    public string? ExtraText { get; set; } = null;
+
+    public bool Published { get; set; } = false;
+
+    public DateTime LastModified { get; set; } = DateTime.UtcNow;
+
+    public List<string> MediaUrls { get; set; } = new();
 }
+
--- Magato.Api/Models/PageContent.cs
+++ Magato.Api/Models/PageContent.cs
@@ -3,7 +3,37 @@ using Microsoft.EntityFrameworkCore;
 namespace Magato.Api.Models;
 public class PageContent
 {
-    public int Id { get; set; }
-    public string Key { get; set; } = null!; // Såsom "AboutMe", "StartPage" osv
-    public string Value { get; set; } = null!; //Innehållet i texten
+    public int Id
+    {
+        get; set;
+    }
+
+    public string Key { get; set; } = null!;
+    public string Title { get; set; } = string.Empty;
+
+    public string? MainText
+    {
+        get; set;
+    }
+    public string? SubText
+    {
+        get; set;
+    }
+    public string? ExtraText
+    {
+        get; set;
+    }
+
+    public bool Published
+    {
+        get; set;
+    }
+
+    public DateTime LastModified
+    {
+        get; set;
+    }
+
+    public List<string> MediaUrls { get; set; } = new();
 }
+
