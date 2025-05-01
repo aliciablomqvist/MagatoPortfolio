c7739fb92d55962dce676179be3870eee507b3cc
Alicia Blomqvist <122750659+aliciablomqvist@users.noreply.github.com>
2025-05-01 13:44:59 +0200

    https fix

--- Magato.Api/obj/Debug/net9.0/Magato.Api.AssemblyInfo.cs
+++ Magato.Api/obj/Debug/net9.0/Magato.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb7d2980edb0b12cec5402cfea7f7ba3aff9f4f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4866516f0caff406ab6765b9e8799005c3d84c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
