44f5657553323922f8db572452919f5b8b368a7d
Alicia Blomqvist <122750659+aliciablomqvist@users.noreply.github.com>
2025-05-16 14:54:53 +0200

    Additional commit

--- Magato.Api/obj/Debug/net9.0/Magato.Api.AssemblyInfo.cs
+++ Magato.Api/obj/Debug/net9.0/Magato.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8638c0641e44182fecb41fd04c3558d827d63e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+318d4899568db87b67f243d5caf952d9a4dc765d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
