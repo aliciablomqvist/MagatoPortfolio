37d5e9fee18adf3fa4b87313cf071cc653fda511
Alicia Blomqvist <122750659+aliciablomqvist@users.noreply.github.com>
2025-05-16 15:58:04 +0200

    Fix end-of-line markers and enforce coding style with .editorconfig

--- Magato.Api/obj/Debug/net9.0/Magato.Api.AssemblyInfo.cs
+++ Magato.Api/obj/Debug/net9.0/Magato.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0442738b88ae2483ace767b0778abc5ff630b1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+449299c6136308499b2729e03c8b9f2ae1434385")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magato.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
